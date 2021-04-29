e50f474166e7137ff81da0da9e04f74ac5f1d528
sathish13898 <sathish13898@gmail.com>
2021-04-29 18:17:44 +0530

    orgin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import Pricecard from './priceCard';
+import './style.css'
 
-function App() {
+
+
+export default function App() {
+  let priceDetails = [
+    {
+      plan : "Free",
+      price : 0,
+      period : 'month',
+      currency : "$",
+      listItems : [
+        {
+          itemName : "Single User",
+          isDisabled : false
+        },
+        {
+          itemName : "5GB Storage",
+          isDisabled : false
+        },
+        {
+          itemName : "Unlimited Public Projects",
+          isDisabled : false
+        },
+        {
+          itemName : "Community Access",
+          isDisabled : false
+        },
+        {
+          itemName : "Unlimited Private Projects",
+          isDisabled : true
+        },
+        {
+          itemName : "Dedicated Phone Support",
+          isDisabled : true
+        },
+        {
+          itemName : "Free Subdomain",
+          isDisabled : true
+        },
+        {
+          itemName : "Monthly Status Reports",
+          isDisabled : true
+        }
+      ]
+    },
+    {
+      plan : "Plus",
+      price : 9,
+      period : 'month',
+      currency : "$",
+      listItems : [
+        {
+          itemName : "5 User",
+          isDisabled : false
+        },
+        {
+          itemName : "50GB Storage",
+          isDisabled : false
+        },
+        {
+          itemName : "Unlimited Public Projects",
+          isDisabled : false
+        },
+        {
+          itemName : "Community Access",
+          isDisabled : false
+        },
+        {
+          itemName : "Unlimited Private Projects",
+          isDisabled : false
+        },
+        {
+          itemName : "Dedicated Phone Support",
+          isDisabled : false
+        },
+        {
+          itemName : "Free Subdomain",
+          isDisabled : false
+        },
+        {
+          itemName : "Monthly Status Reports",
+          isDisabled : true
+        }
+      ]
+    },
+    {
+      plan : "Pro",
+      price : 49,
+      period : 'month',
+      currency : "$",
+      listItems : [
+        {
+          itemName : "Unlimited Users",
+          isDisabled : false
+        },
+        {
+          itemName : "150GB Storage",
+          isDisabled : false
+        },
+        {
+          itemName : "Unlimited Public Projects",
+          isDisabled : false
+        },
+        {
+          itemName : "Community Access",
+          isDisabled : false
+        },
+        {
+          itemName : "Unlimited Private Projects",
+          isDisabled : false
+        },
+        {
+          itemName : "Dedicated Phone Support",
+          isDisabled : false
+        },
+        {
+          itemName : "unlimited Free Subdomain",
+          isDisabled : false
+        },
+        {
+          itemName : "Monthly Status Reports",
+          isDisabled : false
+        }
+      ]
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+         
+        </div>
+        <div className="row">
+          {
+            priceDetails.map((price,index) => {
+              return <Pricecard key={index} details={price}></Pricecard>
+            })
+          }
+        </div>
+      </div>
+    </section>
   );
 }
-
-export default App;
